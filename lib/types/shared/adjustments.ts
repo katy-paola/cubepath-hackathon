11e1841d2744f4898a4714230d923b5ab72b796a
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-30 18:17:52 -0500

    feat(types): add shared types for adjustments, config, and training

---
+++ lib/types/shared/adjustments.ts
@@ -0,0 +1,1 @@
+export const ENERGY_LEVEL_VALUES = ["baja", "media", "alta"] as const;
---
+++ lib/types/shared/training.ts
@@ -0,0 +1,43 @@
+export const DAY_NUMBER_VALUES = [1, 2, 3, 4, 5, 6] as const;
+
+export const TRAINING_TYPE_VALUES = [
+  "suave",
+  "largo",
+  "intervalos",
+  "tempo",
+  "fartlek",
+  "recuperacion",
+  "cruzado",
+  "fuerza",
+  "descanso",
+] as const;
+
+/*
+  El valor se usará para calcular la intensidad de la rutina tomando la que más se repite o el promedio si son iguales
+ */
+export const INTENSITY_VALUES = [
+  {
+    valor: 1,
+    nivel: "baja",
+  },
+  {
+    valor: 2,
+    nivel: "media",
+  },
+  {
+    valor: 3,
+    nivel: "alta",
+  },
+] as const;
+
+/*
+  Un día "por_completar" se refiere a:
+  - El primero de la rutina si ninguno se ha marcado como "completado"
+  - El siguiente después del último día marcado como "completado"
+  Para los días siguientes a este, el estado siempre será "pendiente"
+*/
+export const STATUS_VALUES = [
+  "pendiente",
+  "por_completar",
+  "completado",
+] as const;
